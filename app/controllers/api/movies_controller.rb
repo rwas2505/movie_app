d308be1d1ec0bb6983dcf6c9dc3d6203df182c51
ryan <ryanwaszak@gmail.com>
2020-03-10 17:37:13 -0500

    add working index and show

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,11 @@
 class Api::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    render "index.json.jb"
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   namespace :api do
     get "/actors/:id" => "actors#show"
   end
+
+  namespace :api do
+    get "/movies" => "movies#index"
+    get "/movies/:id" => "movies#show"
+  end
 end
