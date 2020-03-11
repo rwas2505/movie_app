e1da0b07b86778fbb1a84552c4715ffba35e94a1
ryan <ryanwaszak@gmail.com>
2020-03-11 00:08:10 -0500

    add seed_dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-10.times do
-  first_name = FFaker::Name.first_name
-  last_name  = FFaker::Name.last_name
-  known_for  = FFaker::Movie.title 
-  Actor.create(first_name: first_name, last_name: last_name, known_for: known_for)
-end
-
-Movie.create(
-  {
-    title: "Inception", year: 1998, plot: "DB within a DB within a DB within a DREAM"
-  }
-)
-
-Movie.create(
-  {
-    title: "Catch Me if You can", year: 1997, plot: "You can try, but I'm fast"
-  }
-)
-
-Movie.create(
-  {
-    title: "Blow", year: 1996, plot: "George is crazy"
-  }
-)
+Actor.create!([
+  {first_name: "Jermaine", last_name: "Anderson", known_for: "Legend of Diaries"},
+  {first_name: "Deidre", last_name: "Mayert", known_for: "Season of the Jungle"},
+  {first_name: "Mariella", last_name: "Kiehn", known_for: "1011 A.D."},
+  {first_name: "Napoleon", last_name: "McGlynn", known_for: "Red Ninjas 2: Son of Red Ninjas"},
+  {first_name: "Dalton", last_name: "Mueller", known_for: "Blonde Witch"},
+  {first_name: "Yessenia", last_name: "Feeney", known_for: "The Man from Across the Ocean"},
+  {first_name: "Jesusita", last_name: "Jast", known_for: "Tokyo Ninja"},
+  {first_name: "Launa", last_name: "McLaughlin", known_for: "1600 A.D."},
+  {first_name: "Jefferey", last_name: "Cassin", known_for: "Codename: Friday"},
+  {first_name: "Dagmar", last_name: "Miller", known_for: "The Tentacle with a Thousand Faces"},
+  {first_name: "Erwin", last_name: "Parker", known_for: "I am Journals"},
+  {first_name: "Frances", last_name: "Hand", known_for: "Time of the Action Brain"},
+  {first_name: "Dwain", last_name: "Kutch", known_for: "Curse of the Electric Fly"},
+  {first_name: "Lucio", last_name: "Hackett", known_for: "American Imp"},
+  {first_name: "Oda", last_name: "Eichmann", known_for: "The Green Wolf from Across the Ocean"},
+  {first_name: "Stefania", last_name: "Hills", known_for: "Imp 2: Electric Boogaloo"},
+  {first_name: "Renna", last_name: "Kshlerin", known_for: "Forbidden Thief"},
+  {first_name: "Sharleen", last_name: "Quigley", known_for: "A Fistful of Ninja"},
+  {first_name: "Albertha", last_name: "Cassin", known_for: "The Ninjas with a Thousand Faces"},
+  {first_name: "Belva", last_name: "Bogan", known_for: "Hungry Diaries"}
+])
+Movie.create!([
+  {title: "Inception", year: 1998, plot: "DB within a DB within a DB within a DREAM"},
+  {title: "Blow", year: 1996, plot: "George is crazy"},
+  {title: "Catch Me if You can", year: 1998, plot: "You can't catch me, I'm the ginger bread man!"}
+])
